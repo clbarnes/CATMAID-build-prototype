4bc427ecac1eabf5525001bbeee994692985a698
Chris Barnes <barnesc@janelia.hhmi.org>
2019-02-21 17:59:52 -0500

    make dev easier

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
-
-  }
+    // should put exports into global namespace, doesn't seem to
+    // libraryTarget: 'var',
+    // library: '[name]',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ]
 };
