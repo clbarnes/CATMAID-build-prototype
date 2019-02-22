06d9bbef8891e091ebc74f7b58273b2fd268ca3d
Chris Barnes <barnesc@janelia.hhmi.org>
2019-02-22 16:54:31 -0500

    refactor index exports

--- src/index.js
+++ src/index.js
@@ -1,74 +1,126 @@
-// global: project, SkeletonAnnotations, CATMAID_TracingTool
+// global: project, SkeletonAnnotations, CATMAID.{TracingTool, TracingOverlay, Init, skeletonListSources}, glWidget?, CATMAID_CLIENT_VERSION
 
-// order dependent
+import * as catmaid from "./CATMAID.js";
+export let requestQueue = catmaid.requestQueue;
 
-import "./tools.js";
-import "./events.js";
-import "./request.js";
-import {requestQueue} from "./CATMAID_js";
-import "./error.js";
-import "./state.js";
-import "./command.js";
+import * as tools from "./tools.js";
+import * as events from "./events.js";
+import * as request from "./request.js";
+import * as error from "./error.js";
+import * as state from "./state.js";
+import * as command from "./command.js";
+import * as annotations from "./models/annotations.js";
+import * as api from "./models/api.js";
+import * as connectors from "./models/connectors.js";
+import * as dataview from "./models/dataview.js";
+import * as group from "./models/group.js";
+import * as labels from "./models/labels.js";
+import * as landmarks from "./models/landmarks.js";
+import * as neuron from "./models/neuron.js";
+import * as nodes from "./models/nodes.js";
+import * as pointcloud from "./models/pointcloud.js";
+import * as pointset from "./models/pointset.js";
+import * as relations from "./models/relations.js";
+import * as sampler from "./models/sampler.js";
+import * as similarity from "./models/similarity.js";
+import * as skeleton from "./models/skeleton.js";
+import * as volumes from "./models/volumes.js";
+import * as skeleton_source from "./skeleton_source.js";
+import * as datastores from "./datastores.js";
+import * as settings_manager from "./settings-manager.js";
+import * as coalescing_promise_deduplicator from "./coalescing-promise-deduplicator.js";
+import * as color_source from "./color-source.js";
+import * as colorizer from "./colorizer.js";
+import * as connectivity_matrix from "./connectivity_matrix.js";
+import * as constants from "./constants.js";
+import * as cropping from "./cropping.js";
+import * as filter from "./filter.js";
+import * as index from "./index.js";
+import * as io from "./io.js";
+import * as lru_cache from "./lru-cache.js";
+import * as moving_least_squares_transform from "./moving-least-squares-transform.js";
+import * as neuron_name_service from "./neuron_name_service.js";
+import * as serializer from "./serializer.js";
+import * as shader from "./shader.js";
+import * as skeleton_operations from "./skeleton-operations.js";
+import * as skeleton_source_manager from "./skeleton_source_manager.js";
+import * as submitter from "./submitter.js";
+import * as svg_factory from "./svg-factory.js";
+import * as svgutil from "./svgutil.js";
+import * as time_series from "./time-series.js";
+import * as workflow from "./workflow.js";
 
-// order independent within ./models
-import "./models/annotations.js";
-import "./models/api.js";
-import "./models/connectors.js";
-import "./models/dataview.js";
-import "./models/group.js";
-import "./models/labels.js";
-import "./models/landmarks.js";
-import "./models/neuron.js";
-import "./models/nodes.js";
-import "./models/pointcloud.js";
-import "./models/pointset.js";
-import "./models/relations.js";
-import "./models/sampler.js";
-import "./models/similarity.js";
-import "./models/skeleton.js";
-import "./models/volumes.js";
+const module_objs = [
+  catmaid,
+  tools,
+  events,
+  request,
+  error,
+  state,
+  command,
+  annotations,
+  api,
+  connectors,
+  dataview,
+  group,
+  labels,
+  landmarks,
+  neuron,
+  nodes,
+  pointcloud,
+  pointset,
+  relations,
+  sampler,
+  similarity,
+  skeleton,
+  volumes,
+  skeleton_source,
+  datastores,
+  settings_manager,
+  coalescing_promise_deduplicator,
+  color_source,
+  colorizer,
+  connectivity_matrix,
+  constants,
+  cropping,
+  filter,
+  index,
+  io,
+  lru_cache,
+  moving_least_squares_transform,
+  neuron_name_service,
+  serializer,
+  shader,
+  skeleton_operations,
+  skeleton_source_manager,
+  submitter,
+  svg_factory,
+  svgutil,
+  time_series,
+  workflow
+];
 
-import "./skeleton_source.js";
-import "./datastores.js";
-import "./settings-manager.js";
-
-// order independent, probably
+// todo: this might break introspection?
+export const CATMAID = module_objs.reduce((accumulator, currentValue) => {
+  const prefix = "CATMAID_";
+  for (let [key, value] of currentValue.entries()) {
+    if (key.startsWith(prefix)) {
+      accumulator[key.slice(prefix.length)] = value;
+    }
+  }
+}, {});
 
 import {Arbor} from "./Arbor.js";
-import "./active_skeleton.js";
-import "./annotated-skeletons-cache.js";
-import "./annotation-cache.js";
-import "./arbor_parser.js";
-import "./basic_skeleton_source.js";
+export {Arbor};
+
 import {CircuitGraphAnalysis} from "./circuit_graph_analysis.js";
-import "./coalescing-promise-deduplicator.js";
-import "./color-source.js";
-import "./colorizer.js";
-import "./connectivity_matrix.js";
-import "./constants.js";
-import "./cropping.js";
-import "./filter.js";
-import "./index.js";
-import "./io.js";
-import "./lru-cache.js";
-import "./moving-least-squares-transform.js";
-import "./neuron_name_service.js";
-import "./serializer.js";
-import "./shader.js";
-import "./skeleton-operations.js";
-import "./skeleton_source_manager.js";
-import "./submitter.js";
-import "./svg-factory.js";
-import "./svgutil.js";
-import {SynapseClustering} from "./synapse_clustering.js";
-import "./time-series.js";
+export {CircuitGraphAnalysis}
+
 import {InstanceRegistry, fetchSkeletons} from "./utils.js";
-import "./volumes.js";
-import "./workflow.js";
+export {InstanceRegistry, fetchSkeletons};
 
-import {CATMAID} from "./namespace.js";
-
-export {CATMAID, requestQueue, Arbor, CircuitGraphAnalysis, SynapseClustering, InstanceRegistry, fetchSkeletons};
+import {SynapseClustering} from "./synapse_clustering.js";
+export {SynapseClustering};
 
 for (let obj of [window, global]) {
   if (obj) {
