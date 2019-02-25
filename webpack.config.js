a438e72a592ff80d6e50b0a22487ac18cd5bf4c9
Chris Barnes <barnesc@janelia.hhmi.org>
2019-02-25 10:59:58 -0500

    commented-out terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -19,7 +20,8 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: "all"
-    }
+    },
+    // minimizer: [new TerserPlugin({parallel: true, sourceMap: true})]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
